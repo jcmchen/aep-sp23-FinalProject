39411cbeb176df048a25cc38a1386ec076392ab5
Jeremy Chen <jeremychen2@gmail.com>
2023-04-21 05:40:54 -0700

    Perimeter Test

--- src/test/java/edu/berkeley/aep/MyRectangleTest.java
+++ src/test/java/edu/berkeley/aep/MyRectangleTest.java
@@ -9,6 +9,11 @@ public class MyRectangleTest {
         MyRectangle r1 = new MyRectangle(0, 0, 5, 6);
         assertEquals(30, r1.getArea());
     }
+    @Test
+    public void perimeterOfRectangleOneWidthFIVEHeightSIXShouldBeTWENTYTWO(){
+        MyRectangle r1 = new MyRectangle(0, 0, 5, 6);
+        assertEquals(22, r1.getPerimeter());
+    }
 
 
 
