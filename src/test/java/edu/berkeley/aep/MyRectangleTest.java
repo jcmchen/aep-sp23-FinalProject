0a0de954c538725cde92ad09dde6faa79ebb1adf
Jeremy Chen <jeremychen2@gmail.com>
2023-04-27 19:23:35 -0700

    Point Not Contains Test

--- src/test/java/edu/berkeley/aep/MyRectangleTest.java
+++ src/test/java/edu/berkeley/aep/MyRectangleTest.java
@@ -1,8 +1,7 @@
 package edu.berkeley.aep;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class MyRectangleTest {
     @Test
@@ -37,14 +36,17 @@ public class MyRectangleTest {
         assertTrue(r1.overlaps(r1));
     }
     @Test
-    public void rectangleOneWidthFIVEHeightSIXShouldOverlapRectangleThreeWidth(){
+    public void rectangleOneWidthFIVEHeightSIXShouldOverlapRectangleThreeWidthEIGHTHeightFOUR(){
         MyRectangle r1 = new MyRectangle(0, 0, 5, 6);
         MyRectangle r3 = new MyRectangle(0, 0, 8, 4);
         assertTrue(r1.overlaps(r3));
     }
 
-
-
+    @Test
+    public void rectangleOneWidthFIVEHeightSIXShouldNotContainPointTHREEONE(){
+        MyRectangle r1 = new MyRectangle(0, 0, 5, 6);
+        assertFalse(r1.contains(3,1));
+    }
 
 
 }
